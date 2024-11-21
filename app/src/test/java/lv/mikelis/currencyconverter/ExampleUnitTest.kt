bcfb98dc0e60a4dfa9a65c19617bf81adea490ee
Mikelis <you@example.com>
2024-11-21 10:47:21 +0200

    added unit test

--- app/src/test/java/lv/mikelis/currencyconverter/ExampleUnitTest.kt
+++ app/src/test/java/lv/mikelis/currencyconverter/ExampleUnitTest.kt
@@ -1,22 +1,30 @@
 package lv.mikelis.currencyconverter
 
-import android.content.Context
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import io.mockk.coEvery
 import io.mockk.mockk
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import lv.mikelis.currencyconverter.networking.NetworkFactory
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.test.runTest
+import lv.mikelis.currencyconverter.networking.api.CurrencyApiService
 import lv.mikelis.currencyconverter.networking.repository.ApiRepository
+import lv.mikelis.currencyconverter.networking.response.ConversionResponse
 import lv.mikelis.currencyconverter.viewmodel.ConverterViewModel
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.ResponseBody
+import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
+import retrofit2.Response
 
 @OptIn(ExperimentalCoroutinesApi::class)
 class ConverterViewModelTest {
 
     @ExperimentalCoroutinesApi
     @get:Rule
-    var mainCoroutineRule = MainDispatcherRule()
+    val mainCoroutineRule = MainDispatcherRule()
 
     @get:Rule
     val rule = InstantTaskExecutorRule()
@@ -26,9 +34,55 @@ class ConverterViewModelTest {
 
     @Before
     fun setUp() {
-        repository = ApiRepository(Dispatchers.IO, NetworkFactory(mockk<Context>(relaxed = true), false).convertApi())
+        val mockApi = mockk<CurrencyApiService> {
+            coEvery { getConversionRate("USD") } returns Response.success(
+                ConversionResponse(
+                    base = "USD",
+                    date = "",
+                    amount = 1.0,
+                    rates = HashMap<String, Double>()
+                )
+            )
+        }
+        repository = ApiRepository(Dispatchers.IO, mockApi)
         viewModel = ConverterViewModel(repository)
     }
 
+    @Test
+    fun checkIfRequestCreatedCorrectly() = runTest {
+        val response = repository.getConversionRate("USD")
+        assertEquals("USD", response?.base)
+    }
 
+    @Test
+    fun checkIfVmDataChanges() = runTest {
+        val expectedBase = "USD"
+        viewModel.getConversionRate(expectedBase)
+        val result = viewModel.conversionRateFlow.first()
+        assertEquals(expectedBase, result?.base)
+    }
+
+    @Test
+    fun checkIfVmLoadingFalse() = runTest {
+        viewModel.getConversionRate("USD")
+        viewModel.conversionRateFlow.first()
+        assertEquals(false, viewModel.isLoading.value)
+    }
+
+    @Test
+    fun checkIfVmLoadingFalseIfException() = runTest {
+        val mockApi = mockk<CurrencyApiService> {
+            coEvery { getConversionRate("USD") } returns Response.error(400,
+                ResponseBody.create(
+                    "application/json".toMediaTypeOrNull(),
+                    "{\"error\":\"Invalid base currency\"}"
+                )
+            )
+        }
+        val repository = ApiRepository(Dispatchers.IO, mockApi)
+        val viewModel = ConverterViewModel(repository)
+        viewModel.getConversionRate("USD")
+        viewModel.conversionRateFlow.first()
+        assertEquals(false, viewModel.isLoading.value)
+    }
 }
